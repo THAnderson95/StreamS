c8e17803a289bdd20a492cc61e22d7ef53341e89
THAnderson95 <194744982+THAnderson95@users.noreply.github.com>
2025-01-25 11:34:59 +1100

    Added extra dependencies to `package.json`
    - prettier
    - dotenv
    - axios / http-proxy-middleware
    - tailwindcss / postcss / autoprefixer
    - Redux (if i decide to use it).

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://graph.facebook.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
+
+
