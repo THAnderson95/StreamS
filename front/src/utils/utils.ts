c248ec53fa7a6065df8ef78755a5754cc26b1a04
THAnderson95 <194744982+THAnderson95@users.noreply.github.com>
2025-01-26 11:52:55 +1100

    Updated `getReactionSymbol` function to check if it's a reaction, or else return ❓

--- front/src/utils/utils.ts
+++ front/src/utils/utils.ts
@@ -1,5 +1,8 @@
 import { REACTION_TYPE_SYMBOLS, REACTION_TYPES } from "@/constants";
 
-export function getReactionSymbol(reaction: REACTION_TYPES): string {
-    return REACTION_TYPE_SYMBOLS[reaction]
+export function getReactionSymbol(reaction: string): string {
+  if (reaction in REACTION_TYPE_SYMBOLS) {
+    return REACTION_TYPE_SYMBOLS[reaction as REACTION_TYPES];
+  }
+  return "❔";
 }
